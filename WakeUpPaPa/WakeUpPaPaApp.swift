8214aba434c9d3661199cad6c831b2697f001f48
minkyu <elliotelon@gmail.com>
2026-01-06 21:53:02 +0900

    Initial Commit

---
+++ WakeUpPaPa/WakeUpPaPaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeUpPaPaApp.swift
+//  WakeUpPaPa
+//
+//  Created by 김민규 on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeUpPaPaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
